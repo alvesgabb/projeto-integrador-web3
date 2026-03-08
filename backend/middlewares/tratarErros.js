98d449a236a0216f3488363a9790d93962a6239b
jvianney@live.com <jvianney@live.com>
2026-03-07 22:33:12 -0300

    atualizando

--- backend/middlewares/tratarErros.js
+++
@@ -1,8 +0,0 @@
-app.use((err, req, res, next) => {
-  console.error("[ERRO]", err);
-  res.status(500).json({ erro: "Ocorreu um erro no servidor" });
-});
-
-app.listen(port, () =>
-  console.log(`Servidor rodando em http://localhost:${port}`),
-);
