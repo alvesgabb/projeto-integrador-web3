eda1dee74a94f7825e5ecd3840124deaef0e02ca
Clarisse Gondim <claryssegondymp10@gmail.com>
2026-03-14 22:41:30 -0300

    correção do app.js e index.html atualização

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -4,7 +4,7 @@
 import * as api from './api.js';
 import { renderizarReceita } from './ui.js';
 
-async function listarReceitas() {
+async function listaReceitas() {
   try {
     const receitas = await api.getReceitas();
     renderizarReceita(receitas);
@@ -13,33 +13,4 @@ async function listarReceitas() {
   }
 }
 
-async function criarReceita(evento) {
-  //bloqueia esse recarregamento automático
-  evento.preventDefault(); 
-
-  // Inicializei as variaveis pelo id
-  const nome = document.getElementById('nome').value.trim();
-  const ingredientes = document.getElementById('ingredientes').value.trim();
-  const modo_preparo = document.getElementById('modo_preparo').value.trim();
-
-// Validando se as variaveis estão nulas
-  if (!nome || !ingredientes || !modo_preparo) return;
-
-// Chamando api e passando as variaveis na função criar receita.
-  try {
-    await api.criarReceita({ nome, ingredientes, modo_preparo  });
-    await listarReceitas(); // atualiza lista
-    evento.target.reset();
-  } catch (erro) {
-    console.error('Erro ao criar receitas:', erro);
-  }
-}
-
-
-// Submetendo o form e acionando a função criarReceita 
-document
-  .getElementById('form-receita')
-  .addEventListener('submit', criarReceita);
-
-
-renderizarReceita();
+listaReceitas();
