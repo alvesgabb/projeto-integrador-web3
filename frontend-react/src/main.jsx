710cff92e8134be6c59d5ea775829ef0c0cc1fdc
Gabriele Alves <bihalves691@gmail.com>
2026-04-09 18:36:32 -0300

    feat: implementação do ScrollToTp,  NotFound e pequenas correcoes

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 import App from './App'
 import './index.css'
+import { ScrollToTop } from './components/ScrollToTop'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
