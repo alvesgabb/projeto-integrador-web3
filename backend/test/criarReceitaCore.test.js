8ab68f8dcff805d9160e3b7ab977f0607bc860f0
ClarisseGondim <claryssegondymp10@gmail.com>
2026-04-18 14:18:13 -0300

    Update image URL and ingredient list in tests
    Alterações realizadas no arquivo criarReceitaCore.js

--- backend/test/criarReceitaCore.test.js
+++ backend/test/criarReceitaCore.test.js
@@ -7,10 +7,10 @@ describe("criarReceitaCore", () => {
   test("cria receita válida", () => {
     const r = criarReceitaCore({
       nome: "Bolo de Chocolate",
-      imagem: "https://exemplo.com/bolo.jpg",
+      imagem: "https://th.bing.com/th/id/OIP.wNovgchbBdX1gT8vtnj56gHaFj?w=268&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7",
       ingredientes: [
-        "2 xícaras de farinha",
-        "1 xícara de açúcar",
+        "2 xícaras de farinha de trigo",
+        "3 ovos",
         "1 xícara de chocolate em pó"
       ],
       modo_de_preparo: "Misture todos os ingredientes e asse por 40 minutos a 180°C.",
@@ -19,10 +19,10 @@ describe("criarReceitaCore", () => {
     });
 
     expect(r.nome).toBe("Bolo de Chocolate");
-    expect(r.imagem).toBe("https://exemplo.com/bolo.jpg");
+    expect(r.imagem).toBe("https://th.bing.com/th/id/OIP.wNovgchbBdX1gT8vtnj56gHaFj?w=268&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7",);
     expect(r.ingredientes).toEqual([
-      "2 xícaras de farinha",
-      "1 xícara de açúcar",
+      "2 xícaras de farinha de trigo",
+      "3 ovos",
       "1 xícara de chocolate em pó"
     ]);
     expect(r.modo_de_preparo).toBe("Misture todos os ingredientes e asse por 40 minutos a 180°C.");
