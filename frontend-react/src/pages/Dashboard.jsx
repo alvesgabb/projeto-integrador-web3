20fe28ad3d184cebb59a9ad1bc8321d2801cf908
Gabriele Alves <bihalves691@gmail.com>
2026-04-24 15:44:51 -0300

    Sincronizando frontend e backend

--- frontend-react/src/pages/Dashboard.jsx
+++ frontend-react/src/pages/Dashboard.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Dashboard() {
-  return <h1>Área Privada</h1>;
+
+  return (
+    <section className="card" style={{ padding: "2rem" }}>
+    
+      <h1> Olá, Chef!</h1>
+      
+      <p style={{ marginTop: ".5rem", maxWidth: "600px", lineHeight: "1.6" }}>
+        Seja bem-vindo à sua área exclusiva no <strong>Sistema de receitas</strong>. 
+        Aqui você pode gerenciar suas receitas e organizar seus ingredientes e modo de preparo!.
+      </p>
+
+    
+      <div style={{ marginTop: "2rem", display: "flex", gap: ".5rem" }}>
+        <Link to="/receitas" className="btn">Ver receitas</Link>
+        <Link to="/" className="btn btn-outline">Voltar à página inicial</Link>
+      </div>
+    </section>
+  );
 }
